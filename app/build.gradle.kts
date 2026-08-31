ad00d1576dd5a849f14a3b218ccb46662788ae6d
Muhammad Usman <umuhammad766@gmail.com>
2026-08-31 19:50:26 +0100

    setup hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,23 @@
 plugins {
     alias(libs.plugins.android.application)
 
-
     id("com.google.devtools.ksp")
-
-
-
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.example.bulletin"
+
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
         }
+    }
 
-        buildFeatures {
-            viewBinding = true
-            buildConfig = true
-        } }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 
     defaultConfig {
         applicationId = "com.example.bulletin"
@@ -27,28 +26,26 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField(
             "String",
             "NEWS_API_KEY",
-            "\"4adafc67d4497fa34fe411f31e57fd53\""
+            "\"PUT_YOUR_API_KEY_HERE\""
         )
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -57,31 +54,46 @@ android {
 }
 
 dependencies {
-    val room_version = "2.8.4"
+    val roomVersion = "2.8.4"
+    val hiltVersion = "2.60.1"
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    ksp("com.google.dagger:hilt-compiler:$hiltVersion")
+
+    // Retrofit and OkHttp
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.room:room-runtime:${room_version}")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
-    ksp("androidx.room:room-compiler:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    testImplementation("junit:junit:4.13.2")
+
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("io.mockk:mockk:1.13.10")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
-
 }
